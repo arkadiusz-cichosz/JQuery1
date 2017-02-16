e935556b385478a09f502fe0a893edbe64b6c2cf
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-02-16 19:49:23 +0100

    add new code

--- js/jquery_ex1.js
+++ js/jquery_ex1.js
@@ -1,8 +1,19 @@
 $(function() {
 	let span_selector = $('span');
+	let paragraphs = $('p');
+	
 	span_selector.each(function(index, element) {
 		if(index % 2 == 0) {
 			$(element).css('color','red');
 		}
+	});
+
+	paragraphs.each(function(index, element) {
+		let extra_button = '<button class=\"btn\" data-tmp=\"' + index + '\">Click me</button>';
+		$(element).append(extra_button);
+	});
+
+	$('button').click(function() {
+		alert($(this).attr('data-tmp'));
 	});
 });
